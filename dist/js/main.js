ca1c363bfcb2732e6369abbc65be6b8ff817495a
apoloreyes28 <reyesapolinar31@gmail.com>
2024-04-04 14:34:03 -0600

    Menu Content

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,9 @@
 // DOM, seleccionamos al elemento (etiqueta) mediante su .clase ó #id
 const menuBtn = document.querySelector('.menu-btn');
-
 const hamburger = document.querySelector('.menu-btn__burger');
-
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item')
 let showMenu = false;// el menu está cerrado (no muestra nada)
 
 // agregamos un evento
@@ -12,11 +13,21 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
     if(!showMenu) {
         hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+
+        // usamos este bucle para recorrer los elementos para realizar 
+        // la misma acción (repetida en cada uno) de añadir la clase 'open'
+        navItems.forEach(item => item.classList.add('open'));
 
         showMenu = true;// menu abierto (muestra lo que contiene)
     }
     else {
         hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+
+        navItems.forEach(item => item.classList.remove('open'));
 
         showMenu = false;// menu cerrado (no muestra lo que contiene)
     }
